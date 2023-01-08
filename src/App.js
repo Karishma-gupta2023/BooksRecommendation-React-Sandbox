ed29c12fd7a4b19105a3ea16e2b033c190e887f3
karishma <karishmaadore@gmail.com>
2023-01-08 22:15:08 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+const movieDB = {
+  Romantic: [
+    { name: " Sita Ramam", rating: "8.6/10" },
+    { name: "Dilwale Dulhaniya Le Jayenge", rating: "8.0/10" }
+  ],
+
+  Drama: [
+    {
+      name: "The Shawshank Redemption",
+      rating: "9.3/10"
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      rating: "7.6/10"
+    }
+  ],
+  Action: [
+    {
+      name: "K.G.F: Chapter 1",
+      rating: "8.2/10"
+    },
+    {
+      name: "Baahubali: The Beginning",
+      rating: "8.0/10"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("Action");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1> 🎦 goodmovies </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite movies. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(movieDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {movieDB[selectedGenre].map((movie) => (
+            <li
+              key={movie.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {movie.name} </div>
+              <div style={{ fontSize: "smaller" }}> {movie.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
